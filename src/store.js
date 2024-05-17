4dc5b3376ae18c4a3a4de025e1855c403a09fba1
Stephenadebayoy <102865623+Stephenadebayoy@users.noreply.github.com>
2024-05-17 11:37:03 +0100

    ui update on google-map

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
+/** @format */
+
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/AuthSlice";
 import orderReducer from "./features/orderSlice";
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        order: orderReducer,
-    },
+  reducer: {
+    auth: authReducer,
+    order: orderReducer,
+  },
 });
 
 export default store;
